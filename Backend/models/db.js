bfcd75aa256a234d391ed61824142843b6648462
choko0318 <sdhspring@gmail.com>
2024-12-01 16:16:03 +0900

    Update db.js

--- Backend/models/db.js
+++ Backend/models/db.js
@@ -5,8 +5,8 @@ require("dotenv").config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "tjehdgk02!",
-  database: process.env.DB_NAME || "databaseproject",
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
